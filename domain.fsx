caf43922ddec10f9d87aea62260f946d70ed81fc
lucas.saldanha <lk.saldanha@gmail.com>
2023-03-10 22:52:20 +0100

    added get price function

--- domain.fsx
+++ domain.fsx
@@ -1,7 +1,7 @@
-type CoffeeType = Arabica | Robusta | Black | Espresso | Cappuccino | Latte | Mocha | Americano | Macchiato | Frappuccino | IcedCoffee
+type CoffeeType = Arabica | Robusta | Espresso | Cappuccino
 type TeaType = Green | Black | White | Oolong | Herbal
 type JuiceType = Orange | Apple | Strawberry | Lemonade | Pineapple
-type SodaType = Cola | Fanta | Sprite | Pespi
+type SodaType = Cola | Fanta | Sprite
 type MilkType = WholeMilk | SemiSkimmedMilk | SkimmedMilk | AlmondMilk | SoyMilk | CoconutMilk
 
 type Size = Small | Medium | Large
@@ -23,10 +23,11 @@ type MilkRecord = { milkType: MilkType; size: Size }
 type Cash = { amount: float }
 type Card = { cardNumber: string; cvc: string; expiryDate: string }
 type MobilePay = { phoneNumber: string }
+
 type PaymentType = Cash | Card | MobilePay
 
 
-type VIAPerson = { name: string; phoneNumber: string }
+type VIAPerson = { name: string; studentNumber: string }
 type SOSUPerson = { name: string; phoneNumber: string }
 type Customer =
     | VIA of VIAPerson
@@ -36,16 +37,72 @@ type Customer =
 type DrinkRecord = { drinkType: DrinkType; size: Size}
 type Product = Drink of DrinkRecord
 
+type Order = { customer: Customer; drink: DrinkRecord; paymentType: PaymentType }
 
-let drink = { drinkType = Coffee Arabica; size = Small }
+let drinkOrdered = { drinkType = Coffee Arabica; size = Small }
 
 
-type Order = { customer: Customer; drinks: drink list; paymentType: PaymentType }
+let getPrice (product: Product) : float =
+    let coffeePrices = 
+        [ Arabica, 2.0
+          Robusta, 1.5
+          Espresso, 2.5
+          Cappuccino, 3.0 ]
+        |> Map.ofList
+        
+    let teaPrices =
+        [ Green, 1.5
+          Black, 1.0
+          White, 2.0
+          Oolong, 2.5
+          Herbal, 1.5 ]
+        |> Map.ofList
+        
+    let juicePrices =
+        [ Orange, 2.5
+          Apple, 2.0
+          Strawberry, 3.0
+          Lemonade, 2.5
+          Pineapple, 3.5 ]
+        |> Map.ofList
+        
+    let sodaPrices =
+        [ Cola, 1.0
+          Fanta, 1.5
+          Sprite, 1.5 ]
+        |> Map.ofList
+        
+    let milkPrices =
+        [ WholeMilk, 2.0
+          SemiSkimmedMilk, 1.5
+          SkimmedMilk, 1.0
+          AlmondMilk, 3.0
+          SoyMilk, 2.5
+          CoconutMilk, 3.5 ]
+        |> Map.ofList
+    
+    let calculatePrice (subType: 'a) (prices: Map<'a, float>) (size: Size) : float =
+        match prices.TryFind(subType) with
+        | Some price ->
+            match size with
+            | Small -> price * 0.8
+            | Medium -> price
+            | Large -> price * 1.2
+        | None -> failwith "Invalid subtype"
+    
+    match product with
+    | Drink { drinkType = Coffee coffeeType; size = size } ->
+        calculatePrice coffeeType coffeePrices size
+    | Drink { drinkType = Tea teaType; size = size } ->
+        calculatePrice teaType teaPrices size
+    | Drink { drinkType = Juice juiceType; size = size } ->
+        calculatePrice juiceType juicePrices size
+    | Drink { drinkType = Soda sodaType; size = size } ->
+        calculatePrice sodaType sodaPrices size
+    | Drink { drinkType = Milk milkType; size = size } ->
+        calculatePrice milkType milkPrices size
 
-let calculateProductPrice :DrinkType =
-    match a with
-    | Coffee -> calCCoffee a
-    | Tea -> calTea a
 
-let calCCoffee typeOfCoffe =
-    match cRec.typeOfCoffe
+let myCoffee = { drinkType = Coffee Arabica; size = Medium }
+let price = getPrice (Drink myCoffee)
+printfn "Price: $%.2f" price
