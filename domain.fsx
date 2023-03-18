66b7d5dd138cf9836c53da9f89028d69222e64ab
lucas-saldanha <lk.saldanha@gmail.com>
2023-03-18 14:42:14 +0100

    refactored and added VAT calculation and orderAgent

--- domain.fsx
+++ domain.fsx
@@ -1,11 +1,15 @@
+// size types
+type Size = 
+    | Small 
+    | Medium 
+    | Large
+
+// drink types
 type CoffeeType = Arabica | Robusta | Espresso | Cappuccino
 type TeaType = Green | Black | White | Oolong | Herbal
 type JuiceType = Orange | Apple | Strawberry | Lemonade | Pineapple
 type SodaType = Cola | Fanta | Sprite
 type MilkType = WholeMilk | SemiSkimmedMilk | SkimmedMilk | AlmondMilk | SoyMilk | CoconutMilk
-
-type Size = Small | Medium | Large
-
 type DrinkType = 
     | Coffee of CoffeeType
     | Tea of TeaType
@@ -13,36 +17,58 @@ type DrinkType =
     | Soda of SodaType
     | Milk of MilkType
 
-type CoffeeRecord = { coffeeType: CoffeeType; size: Size }
-type TeaRecord = { teaType: TeaType; size: Size; }
-type JuiceRecord = { juiceType: JuiceType; size: Size }
-type SodaRecord = { sodaType: SodaType; size: Size }
-type MilkRecord = { milkType: MilkType; size: Size }
+// record types for each drink type
+type CoffeeRecord = 
+    { Type : CoffeeType; Size : Size }
+type TeaRecord = 
+    { Type : TeaType; Size : Size }
+type JuiceRecord = 
+    { Type : JuiceType; Size : Size }
+type SodaRecord = 
+    { Type : SodaType; Size : Size }
+type MilkRecord = 
+    { Type : MilkType; Size : Size }
 
 
-type Cash = { amount: float }
-type Card = { cardNumber: string; cvc: string; expiryDate: string }
-type MobilePay = { phoneNumber: string }
-
-type PaymentType = Cash | Card | MobilePay
 
 
+
+// payment types
+type CashRecord = unit
+type CardRecord = { cardNumber: string; cvc: string; expiryDate: string }
+type MobilePayRecord = { phoneNumber: string }
+type PaymentType = 
+    | Cash of CashRecord
+    | Card of CardRecord
+    | MobilePay of MobilePayRecord
+
+
+
+// customer types
 type VIAPerson = { name: string; studentNumber: string }
 type SOSUPerson = { name: string; phoneNumber: string }
 type Customer =
     | VIA of VIAPerson
     | SOSU of SOSUPerson
-    
-
-type DrinkRecord = { drinkType: DrinkType; size: Size}
-type Product = Drink of DrinkRecord
-
-type Order = { customer: Customer; drink: DrinkRecord; paymentType: PaymentType }
-
-let drinkOrdered = { drinkType = Coffee Arabica; size = Small }
 
 
-let getPrice (product: Product) : float =
+
+// product types
+type DrinkRecord = { DrinkType: DrinkType; Size: Size}
+type Product = 
+    | Drink of DrinkRecord
+
+// record type for a single drink order and its quantity
+type DrinkOrder = { Drink : DrinkRecord; Quantity : int }
+
+
+// order type
+type Order = { Customer: Customer; Drink: DrinkRecord; Quantity: int; Payment: PaymentType }
+
+
+
+// function to calculate unit price of product based on Product type and size
+let getUnitPrice (product: Product) : float =
     let coffeePrices = 
         [ Arabica, 2.0
           Robusta, 1.5
@@ -91,18 +117,60 @@ let getPrice (product: Product) : float =
         | None -> failwith "Invalid subtype"
     
     match product with
-    | Drink { drinkType = Coffee coffeeType; size = size } ->
+    | Drink { DrinkType = Coffee coffeeType; Size = size } ->
         calculatePrice coffeeType coffeePrices size
-    | Drink { drinkType = Tea teaType; size = size } ->
+    | Drink { DrinkType = Tea teaType; Size = size } ->
         calculatePrice teaType teaPrices size
-    | Drink { drinkType = Juice juiceType; size = size } ->
+    | Drink { DrinkType = Juice juiceType; Size = size } ->
         calculatePrice juiceType juicePrices size
-    | Drink { drinkType = Soda sodaType; size = size } ->
+    | Drink { DrinkType = Soda sodaType; Size = size } ->
         calculatePrice sodaType sodaPrices size
-    | Drink { drinkType = Milk milkType; size = size } ->
+    | Drink { DrinkType = Milk milkType; Size = size } ->
         calculatePrice milkType milkPrices size
 
 
-let myCoffee = { drinkType = Coffee Arabica; size = Medium }
-let price = getPrice (Drink myCoffee)
-printfn "Price: $%.2f" price
+// function to calculate the price of an entire drink order
+let getOrderPrice (order: DrinkOrder) : float =
+    let unitPrice = getUnitPrice (Drink order.Drink)
+    let totalQuantity = float order.Quantity
+    let totalPrice = unitPrice * totalQuantity
+    totalPrice
+
+// function to add VAT to a given price
+let addVAT (price: float) : float =
+    let vat = 0.25
+    price + (price * vat)
+
+
+// order drink
+type OrderDrinkMsg = 
+    | OrderDrink of DrinkType * Size * int
+    | LeaveAComment of string
+
+// order agent
+// Define a mailbox processor to handle messages for ordering drinks and leaving comments
+let orderAgent =
+    MailboxProcessor.Start(fun inbox ->
+        let rec loop () =
+            async {
+                let! msg = inbox.Receive()
+                match msg with
+                | OrderDrink(drinkType: DrinkType, size: Size, quantity) ->
+                    printfn "Ordering %d of %A (%A)" quantity drinkType size
+                    return! loop()
+                | LeaveAComment(comment) ->
+                    printfn "Leaving a comment: %s" comment
+                    return! loop()
+            }
+        loop()
+    )
+
+
+// send a message to the orderAgent to order a drink
+orderAgent.Post(OrderDrink(Coffee Arabica, Small, 2));;
+orderAgent.Post(OrderDrink(Tea Green, Large, 1));;
+orderAgent.Post(OrderDrink(Juice Orange, Medium, 3));;
+
+// send a message to the orderAgent to leave a comment
+orderAgent.Post(LeaveAComment "This is a comment")
+
